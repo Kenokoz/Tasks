2abfdf56429e926cda44e49ac57497f445838383
Kirill Andreev <kenokoz@yandex.ru>
2021-04-12 12:41:41 +0300

    Third task

--- Tasks.js
+++ Tasks.js
@@ -28,3 +28,28 @@ str3.isPalindrom(); // false
 console.log(`Second task - ${str1.isPalindrom()}`);
 console.log(`Second task - ${str2.isPalindrom()}`);
 console.log(`Second task - ${str3.isPalindrom()}`);
+
+// Third task
+// ES5
+function HumanES5() {
+}
+
+function ManES5(name) {
+  this.name = name;
+}
+
+ManES5.prototype = Object.create(HumanES5.prototype);
+ManES5.prototype.constructor = HumanES5;
+
+// ES6
+class HumanES6 {
+}
+
+class ManES6 extends HumanES6 {
+  constructor(name) {
+    super();
+    this.name = name;
+  }
+};
+
+const man = new ManES6('Es6')
