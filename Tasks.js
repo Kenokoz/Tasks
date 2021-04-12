1234ab0e6d2859a58e4302ad9df5d05ef2e6dcfe
Kirill Andreev <kenokoz@yandex.ru>
2021-04-12 12:00:40 +0300

    Second task

--- Tasks.js
+++ Tasks.js
@@ -7,4 +7,24 @@ function sum() {
 Array.prototype.sum = sum;
 
 arr.sum(); // 5
-console.log(arr.sum());
+console.log(`First task - ${arr.sum()}`);
+
+//Second task
+function isPalindrom() {
+  const str = this.toLowerCase().split(' ').join('').split('');
+  return str.join('') === str.reverse().join('');
+}
+
+String.prototype.isPalindrom = isPalindrom;
+
+const str1 = 'Иди';
+const str2 = 'А роза упала на лапу Азора';
+const str3 = 'Человек';
+
+str1.isPalindrom(); // true
+str2.isPalindrom(); // true
+str3.isPalindrom(); // false
+
+console.log(`Second task - ${str1.isPalindrom()}`);
+console.log(`Second task - ${str2.isPalindrom()}`);
+console.log(`Second task - ${str3.isPalindrom()}`);
