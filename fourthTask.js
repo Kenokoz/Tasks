f806711f064fdd531e3c45f3b656858b264ff030
Kirill Andreev <kenokoz@yandex.ru>
2021-04-13 10:20:53 +0300

    Change the context

--- fourthTask.js
+++ fourthTask.js
@@ -1,23 +1,25 @@
 //Fourth task
 function add() {
-  return this.a === 0 || this.a ? ++this.a : this + 1;
+  this.a++;
+  return this;
 }
 
 function dec() {
-  return this.a === 0 || this.a ? --this.a : this - 1
+  this.a--;
+  return this;
 }
 
+function showResult() {
+  console.log(obj.a);
+}
 
-Number.prototype.add = add;
 Object.prototype.add = add;
-
-Number.prototype.dec = dec;
 Object.prototype.dec = dec;
 
 const obj = {
 	a: 0
 };
 
-// obj.add().add().dec().add(); // 2
+obj.add().add().dec().add(); // 2
 
-console.log(obj.add().add().dec().add()); // 2
+showResult();
